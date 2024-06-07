e1ff5a6f07f5451f38aa20bd11a391c15170b42c
Aakash Deep <7773aakashsharma@gmail.com>
2024-06-07 14:17:21 +0530

    function basic

--- js/10_functions.js
+++ js/10_functions.js
@@ -1,77 +1,77 @@
-// let userName = 'yash';
-// function showMessage() {
-//   userName = "jay";
+let userName = 'yash';
+function showMessage() {
+  userName = "jay";
 
-//   let message = 'Hello, ' + userName;
-//   alert(message);
-// }
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
 
-// alert( userName ); // yash be
-// showMessage();
-// alert( userName );
+alert( userName ); // yash be
+showMessage();
+alert( userName );
 
-// //important
+//important
 
-// let username = 'John';
-// function showMessage() {
-//   let username = "Bob"; // declare a local variable
+let username = 'John';
+function showMessage() {
+  let username = "Bob"; // declare a local variable
 
-//   let message = 'Hello, ' + username; // Bob
-//   alert(message);
-// }
-// // the function will create and use its own username
-// showMessage();
-// alert( username );
+  let message = 'Hello, ' + username; // Bob
+  alert(message);
+}
+// the function will create and use its own username
+showMessage();
+alert( username );
 
-// // parameter
-// function showMessage(from, text) {
-//     from = '*' + from + '*'; // make "from" look nicer
-//     alert( from + ': ' + text );
-//   }
-//   let from = "Ajay";
-//   showMessage(from, "Hello"); // *Ajay*: Hello
-//   // the value of "from" is the same, the function modified a local copy
-//   alert( from ); // Ajay
+// parameter
+function showMessage(from, text) {
+    from = '*' + from + '*'; // make "from" look nicer
+    alert( from + ': ' + text );
+  }
+  let from = "Ajay";
+  showMessage(from, "Hello"); // *Ajay*: Hello
+  // the value of "from" is the same, the function modified a local copy
+  alert( from ); // Ajay
 
-//   // note:If a function is called, but an argument is not provided, then the corresponding value becomes undefined.
-//   // A function with an empty return or without it returns undefined
+  // note:If a function is called, but an argument is not provided, then the corresponding value becomes undefined.
+  // A function with an empty return or without it returns undefined
 
-//   function showCount(count) {
-//     // if count is undefined or null, show "unknown"
-//     alert(count ?? "unknown");
-//   }
-//   showCount(0); // 0
-//   showCount(null); // unknown
-//   showCount(); // unknown
+  function showCount(count) {
+    // if count is undefined or null, show "unknown"
+    alert(count ?? "unknown");
+  }
+  showCount(0); // 0
+  showCount(null); // unknown
+  showCount(); // unknown
   
 
-//   function checkAge(age) {
-//     if (age >= 18) {
-//       return true;
-//     } else {
-//       return confirm('Do you have permission from your parents?');
-//     }
-//   }
-//   let age = prompt('How old are you?', 18);
-//   if ( checkAge(age) ) {
-//     alert( 'Hello' );
-//   } else {
-//     alert( 'Hey' );
-//   }
+  function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  let age = prompt('How old are you?', 18);
+  if ( checkAge(age) ) {
+    alert( 'Hello' );
+  } else {
+    alert( 'Hey' );
+  }
 
-//  // to find prime numbers upto n;
-//  function showPrimes(n) {
-//     for (let i = 2; i < n; i++) {
-//       if (!isPrime(i)) continue;
-//         alert(i);  // a prime
-//     }
-//   }
-//   function isPrime(n) {
-//     for (let i = 2; i < n; i++) {
-//       if ( n % i == 0) return false;
-//     }
-//     return true;
-//   }
+ // to find prime numbers upto n;
+ function showPrimes(n) {
+    for (let i = 2; i < n; i++) {
+      if (!isPrime(i)) continue;
+        alert(i);  // a prime
+    }
+  }
+  function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+      if ( n % i == 0) return false;
+    }
+    return true;
+  }
 
   // 
   function pow(a,b) {
@@ -81,7 +81,7 @@
     }
     return result;
   }
-  let a = prompt("x?", '');
+  let a = prompt("a?", '');
   let b = prompt("b?", '');
 
     alert( pow(a,b) );
